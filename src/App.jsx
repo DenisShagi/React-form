ca8365722cc6fef45925e1a5c2e3b438c4d1d802
Denis <denis.shagim@icloud.com>
2024-12-05 04:05:04 +0500

    refactor: delete styles is defined but never used  no-unused-vars

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { Form } from "./components/Form";
-import styles from "./App.module.scss";
 
 function App() {
   return (
